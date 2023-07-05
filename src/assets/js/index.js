0ac47d0e8f2e96dde7164b26ff9b0a34224921d8
michelle-mcn <138590244+michelle-mcn@users.noreply.github.com>
2023-07-05 12:28:27 +0100

    feat: create quiz topics DOM elements from quiz

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -51,3 +51,81 @@ function shuffleArray(array) {
   }
   return array;
 }
+
+// DOM Elements
+const quizHeadingEl = document.querySelector("#quiz-heading");
+const quizQuestionEl = document.querySelector("#quiz-question");
+
+const quizTopicSelectEl = document.querySelector("#quiz-topic-select");
+const quizUlEl = document.querySelector("#quiz-options-list");
+
+// DOM Elements
+const quizSelectEl = document.querySelector("#quiz-topic-select");
+const quizTopicsContainerEl = document.querySelector("#quiz-topics-lg-screen");
+
+/**
+ *  Create topic cards for each quiz topic
+ * @param {String} topic - Quiz topic name
+ * @param {Number} index - Obtained when passing createTopic function to forEach method
+ * @param {String} quizDescription - Quiz topic  description
+ */
+function createTopicCardElements(topic, index, quizDescription) {
+  // Create Topic Card Elements
+  const topicCardButton = document.createElement("button");
+  const topicCardHeading = document.createElement("span");
+  const topicCardText = document.createElement("span");
+  const topicImgEl = document.createElement("img");
+
+  // Add Content
+  topicCardHeading.textContent = topic.replace(/_/g, " ");
+  topicCardText.textContent = quizDescription[index];
+
+  // Set Attributes
+  topicCardButton.setAttribute("data", topic);
+  topicCardButton.setAttribute("aria-label", `Select ${topic} `);
+  topicImgEl.setAttribute(
+    "src",
+    `../assets/images/icons/${topic.replace(/_/g, "-")}.svg`
+  );
+  topicImgEl.setAttribute("alt", `${topic} icon`);
+  topicImgEl.setAttribute("width", "24");
+  topicImgEl.setAttribute("height", "24");
+  topicImgEl.setAttribute("role", "presentation");
+
+  // Append Elements to DOM
+  topicCardButton.appendChild(topicCardHeading);
+  topicCardButton.appendChild(topicCardText);
+  topicCardButton.appendChild(topicImgEl);
+  quizTopicsContainerEl.appendChild(topicCardButton);
+}
+
+
+function createTopicSelectElements(topic) {
+  // Create Topic Select Elements
+  const topicOption = document.createElement("option");
+
+  // Add Content to Elements
+  topicOption.textContent = topic
+  topicOption.textContent = formatString(topic);
+
+  // Set Attributes
+  topicOption.setAttribute("value", topic);
+  topicOption.setAttribute("aria-label", `Select ${topic} `);
+
+  // Append Elements to DOM
+  quizSelectEl.appendChild(topicOption);
+}
+
+// Replace underscores with spaces & capitalise First Letter
+function formatString(str) {
+  return str.replace(/(^|_)\w/g, (letter) => letter.toUpperCase().replace('_', " "));
+}
+
+
+const quizTopicsSorted = quizTopics.sort();
+// Create Topic Elements for Small & Large Screens
+quizTopicsSorted.forEach((topic, index) => {
+  // access topic description from data file
+  createTopicSelectElements(topic);
+  createTopicCardElements(topic, index, quizDescriptions);
+});
