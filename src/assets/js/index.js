bd4080393049f1ec5a8f8d9388c7d4c58e4a06c2
michelle-mcn <138590244+michelle-mcn@users.noreply.github.com>
2023-07-10 17:28:43 +0100

    feat: update toast message Updates toast message in DOM when player makes changes to player setting

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -469,6 +469,38 @@ function setGameOverMessage(isAnswerCorrect) {
 }
 
 /**
+ * @description - Updates the toast messages in the DOM
+ * @param {Object} message
+ * @param {'easy'|'medium'|'hard'} [message.difficulty_level] - The difficulty level of the game.
+ * @param {'on'} [message.reset_score] - The reset score value.
+ */
+function updateToast(message) {
+  const { difficulty_level, reset_score } = message;
+  toastContainerEl.classList.remove("hidden");
+  toastContainerEl.classList.add("fixed");
+
+  if (difficulty_level) {
+    // clone list and append to DOM
+    const cloneListEl = toastListEl.cloneNode(true);
+    cloneListEl.textContent = `Difficulty level set to ${difficulty_level}`;
+    toastEl.appendChild(cloneListEl);
+  }
+
+  // add reset score message to toast and line break
+  if (reset_score) {
+    const cloneListEl = toastListEl.cloneNode(true);
+    cloneListEl.textContent = "Score reset";
+    toastEl.appendChild(cloneListEl);
+  }
+
+  setTimeout(() => {
+    toastContainerEl.classList.add("hidden");
+    toastContainerEl.classList.remove("fixed");
+    toastEl.textContent = "";
+  }, toastMessageTime);
+}
+
+/**
  * @description - Updates the screen reader text for player lives
  */
 
