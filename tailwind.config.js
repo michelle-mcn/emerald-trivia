c12e541298a713765b2bed35d984886b0ed9a07f
michelle-mcn <138590244+michelle-mcn@users.noreply.github.com>
2023-07-11 10:07:44 +0100

    feat: extend tailwindcss animations add additional animations for player lives icons and loading animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'ping-slow': 'ping 2.5s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'ping-new': 'ping 1s cubic-bezier(0, 0, 0.2, 1) normal forwards',
+      },
+      keyframes: {
+        "pulse-slow": {
+          '75%, 100%': {transform: 'scale(2)',opacity: '0',}
+        },
+        "pulse-new": {
+          '75%, 100%': {transform: 'scale(2)',opacity: '0',}
+        }
+      }
+    },
   },
   plugins: [],
 }
